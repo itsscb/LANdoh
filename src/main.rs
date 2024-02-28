af2ec8aeee99caf91b75ecdccfa14474fa24e999
itsscb <dev@itsscb.de>
2024-02-28 22:24:33 +0100

    rf/refactors protos & server (partial)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use landoh::server::Server;
+use landoh::server::ServerOld;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let _ = read_files("testdir");
 
     let addr = "0.0.0.0:9001".parse()?;
-    let sv = Server::default();
+    let sv = ServerOld::default();
     sv.serve(addr).await?;
 
     Ok(())
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::{
     error::Error,
+    fmt::format,
     fs::File,
     io::{BufReader, Read},
     net::SocketAddr,
@@ -23,15 +24,98 @@ mod landoh_proto {
         tonic::include_file_descriptor_set!("landoh_descriptor");
 }
 
-type ResponseStream = Pin<Box<dyn Stream<Item = Result<FileResponse, Status>> + Send>>;
-type DirectoryResult<T> = Result<Response<T>, Status>;
+use pb_proto::{
+    get_file_response::FileResponse as frep, lan_doh_server, FileMetaData, GetDirectoryRequest,
+    GetDirectoryResponse, GetFileRequest, GetFileResponse,
+};
+
+use self::pb_proto::lan_doh_server::LanDoh;
+mod pb_proto {
+    include!("pb.rs");
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("pb_descriptor");
+}
 
 #[derive(Debug, Default)]
 pub struct Server;
 
+impl Server {
+    pub async fn serve(&self, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+        let reflection_service = tonic_reflection::server::Builder::configure()
+            .register_encoded_file_descriptor_set(pb_proto::FILE_DESCRIPTOR_SET)
+            .build()
+            .unwrap();
+
+        tServer::builder()
+            .add_service(lan_doh_server::LanDohServer::new(Self))
+            .add_service(reflection_service)
+            .serve(addr)
+            .await?;
+        Ok(())
+    }
+}
+
 #[tonic::async_trait]
-impl landoh_server::Landoh for Server {
-    type GetDirectoryStream = ResponseStream;
+impl LanDoh for Server {
+    type GetFileStream = Pin<Box<dyn Stream<Item = Result<GetFileResponse, Status>> + Send>>;
+
+    async fn get_directory(
+        &self,
+        request: Request<GetDirectoryRequest>,
+    ) -> Result<Response<GetDirectoryResponse>, Status> {
+        let r = request.into_inner();
+        let path = PathBuf::from(r.name.clone());
+        if !path.exists() {
+            return Err(Status::not_found(format!(
+                "directory not found: {}",
+                r.name
+            )));
+        }
+
+        let mut files: Vec<String> = vec![];
+
+        for f in WalkDir::new(&path) {
+            let e = f.unwrap();
+
+            if e.metadata().unwrap().is_dir() {
+                continue;
+            }
+
+            files.push(String::from(e.path().to_str().unwrap()));
+        }
+
+        // unimplemented!("{}", "get_directory");
+        Ok(Response::new(GetDirectoryResponse { path: files }))
+    }
+
+    async fn get_file(
+        &self,
+        request: Request<GetFileRequest>,
+    ) -> Result<Response<Self::GetFileStream>, Status> {
+        unimplemented!("{}", "get_file");
+        // Ok(Response::new(Box::pin(GetFileResponse {
+        //     file_response: Some(frep::MetaData(FileMetaData {
+        //         file_size: 20 as u32,
+        //         hash: "blub".to_string(),
+        //     })),
+        // }))) as Self::GetFileStream
+        let (tx, rx) = mpsc::channel(128);
+
+        let output_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(Box::pin(output_stream) as Self::GetFileStream))
+    }
+}
+
+type FileResponseStream = Pin<Box<dyn Stream<Item = Result<FileResponse, Status>> + Send>>;
+type DirectoryResult<T> = Result<Response<T>, Status>;
+
+#[derive(Debug, Default)]
+pub struct ServerOld;
+
+#[tonic::async_trait]
+impl landoh_server::Landoh for ServerOld {
+    type GetDirectoryStream = FileResponseStream;
 
     async fn get_directory(
         &self,
@@ -45,7 +129,7 @@ impl landoh_server::Landoh for Server {
         let (tx, rx) = mpsc::channel(128);
 
         tokio::spawn(async move {
-            let _ = Server::send_dir(&Self, path.to_str().unwrap(), tx).await;
+            let _ = ServerOld::send_dir(&Self, path.to_str().unwrap(), tx).await;
         });
 
         let output_stream = ReceiverStream::new(rx);
@@ -55,7 +139,7 @@ impl landoh_server::Landoh for Server {
     }
 }
 
-impl Server {
+impl ServerOld {
     pub async fn serve(&self, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
         let reflection_service = tonic_reflection::server::Builder::configure()
             .register_encoded_file_descriptor_set(landoh_proto::FILE_DESCRIPTOR_SET)

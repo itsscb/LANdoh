3431bf882cb5a0eb14a6a9e97ce1bb737ae822ff
itsscb <dev@itsscb.de>
2024-04-01 22:49:52 +0200

    ft/adds exit channel

--- src-tauri/src/sv.rs
+++ src-tauri/src/sv.rs
@@ -1,21 +1,26 @@
-use std::{error::Error, future::IntoFuture, path::PathBuf, pin::Pin};
-#[allow(unused_imports)]
+use std::thread;
+use std::time::Duration;
+use std::{env, path::PathBuf, pin::Pin};
 use std::{net::{Ipv4Addr, SocketAddr}, sync::{Arc, RwLock}};
 
-use tokio::sync::{mpsc::{self, Receiver, Sender}, Mutex};
-use tokio::sync::oneshot::{self, Sender as oSender, Receiver as oReceiver};
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::oneshot::{self, Sender as oSender};
+use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::Stream;
 use tonic::{async_trait, Request as tRequest, Response as tResponse, Status, transport::Server as tServer};
 use uuid::Uuid;
 
-#[allow(unused_imports)]
-#[cfg(not(test))]
-use log::{debug, error, info, warn};
- 
+
 #[allow(unused_imports)]
 #[cfg(test)]
 use std::{println as info, println as warn, println as debug, println as error};
 
+#[allow(unused_imports)]
+#[cfg(not(test))]
+use log::{debug, error, info, warn};
+
+use self::pb::get_file_response::FileResponse;
+use self::pb::FileMetaData;
 use self::pb::{Directory, lan_doh_server::LanDoh, GetDirectoryRequest, GetDirectoryResponse, GetFileRequest, GetFileResponse, ListDirectoriesRequest, ListDirectoriesResponse};
 
 mod pb {
@@ -27,6 +32,7 @@ mod pb {
 #[derive(Debug)]
 pub enum Message {
     AddDir(PathBuf),
+    SendFile(String),
     RemoveDir(PathBuf),
     SetNickname(String),
     StopBroadcast,
@@ -46,7 +52,7 @@ pub enum Response {
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-struct Sv {
+pub struct Sv {
     id: String,
     nickname: Arc<RwLock<String>>,
     shared: Arc<RwLock<Vec<Directory>>>,
@@ -67,43 +73,69 @@ impl LanDoh for Sv {
         &self,
         _request: tRequest<ListDirectoriesRequest>,
     ) -> Result<tResponse<ListDirectoriesResponse>, Status> {
-        debug!("SV::LIST_DIRECTORIES");
-        Ok(tResponse::new(ListDirectoriesResponse {
-            dirs: vec![] }))
+        unimplemented!("SV::LIST_DIRECTORIES");
+        // Ok(tResponse::new(ListDirectoriesResponse {
+        //     dirs: vec![] }))
         }
     async fn get_directory(
         &self,
-        request: tRequest<GetDirectoryRequest>,
+        _request: tRequest<GetDirectoryRequest>,
     ) -> Result<tResponse<GetDirectoryResponse>, Status> {
-        Ok(tResponse::new(GetDirectoryResponse{files: vec![]}))
-        // unimplemented!("SV::GET_DIRECTORY");
+        // Ok(tResponse::new(GetDirectoryResponse{files: vec![]}))
+        unimplemented!("SV::GET_DIRECTORY");
     }
 
     async fn get_file(
         &self,
         request: tRequest<GetFileRequest>,
     ) -> Result<tResponse<Self::GetFileStream>, Status> {
-        unimplemented!("SV::GET_FILE");
+        let (tx, rx): (
+            Sender<Result<GetFileResponse, Status>>,
+            Receiver<Result<GetFileResponse, Status>>,
+        ) = mpsc::channel(128);
+
+        info!("Got 'GetFile' Request: {:?}", request);
+
+        // let txx = self.tx.clone();
+        tokio::spawn(async move {
+            let path = "pseudo_path".to_string();
+            let hash = "psuedo_hash".to_string();
+
+            loop {
+                // let _ = txx.send(Message::SendFile(path.clone())).await;
+                let _ = tx.send(Ok(GetFileResponse {
+                    file_response: Some(FileResponse::Meta(FileMetaData{
+                        file_size: 0,
+                        path: path.clone(),
+                        hash: hash.clone(),
+                    }))
+                })).await;
+            }
+        });
+
+        let output_stream: ReceiverStream<Result<GetFileResponse, Status>> =
+            ReceiverStream::new(rx);
+
+        Ok(tResponse::new(Box::pin(output_stream) as Self::GetFileStream))
     }
 }
 
 
 #[allow(dead_code)]
-    pub async fn serve(sv: Sv, addr: SocketAddr, exit: oReceiver<()>) -> Result<(), ()> {
-        let reflection_service = tonic_reflection::server::Builder::configure()
+    pub fn serve(sv: Sv, addr: SocketAddr) -> oSender<()> {
+        let (tx, rx) = oneshot::channel::<()>();
+        tokio::spawn(async move {
+            let reflection_service = tonic_reflection::server::Builder::configure()
             .register_encoded_file_descriptor_set(pb::FILE_DESCRIPTOR_SET)
             .build()
             .unwrap();
-
-        let res = tServer::builder()
+        
+            let _ = tServer::builder()
             .add_service(pb::lan_doh_server::LanDohServer::new(sv))
             .add_service(reflection_service)
-            .serve_with_shutdown(addr, async { drop(exit) })
-            // .serve(addr)
-            .await;
-
-        debug!("SERVE: {:?}", res);
-        Ok(())
+            .serve_with_shutdown(addr, async { drop(rx.await)} ).await;        
+    });
+        tx
     }
 
 #[allow(dead_code)]
@@ -119,7 +151,7 @@ pub struct SvBuilder {
 impl SvBuilder {
     #[allow(dead_code)]
     pub fn new(nickname: String, shared: Vec<String>) -> Self {
-        let (tx, rx) = mpsc::channel::<Message>(32);
+        let (tx, rx) = mpsc::channel::<Message>(128);
         let nick = Arc::new(RwLock::new(nickname));
         let dirs = Arc::new(RwLock::new(shared
             .iter()
@@ -141,7 +173,7 @@ impl SvBuilder {
 
 #[allow(dead_code)]
 pub async fn run(self) -> (Sender<Message>, Receiver<Response>){
-    let (reply_tx, reply_rx) = mpsc::channel::<Response>(32);
+    let (reply_tx, reply_rx) = mpsc::channel::<Response>(128);
 
     let nickname = self.nickname;
     let shared = self.shared;
@@ -150,9 +182,9 @@ pub async fn run(self) -> (Sender<Message>, Receiver<Response>){
     let tx = self.manager_tx;
     let atx = tx.clone();
     let _ = tokio::spawn(async move {
-        // let mut serve_exit_tx: Arc<Mutex<Option<oSender<()>>>> = Arc::new(Mutex::new(None));
-        // let serve_exit_fn = None;
+        let mut serve_exit_tx: Option<oSender<()>> = None;
         let mut rx = rx;
+        let tx = atx.clone();
         let rtx = reply_tx;
         let id = id;
         let sv = Sv{
@@ -165,9 +197,13 @@ pub async fn run(self) -> (Sender<Message>, Receiver<Response>){
             match rx.recv().await {
                 Some(msg) => {
                     match msg {
+                        Message::SendFile(p) => {
+                            debug!("Got 'SendFile' Message: {}", &p);
+                            let _ = rtx.send(Response::Done(Message::SendFile(p))).await;
+                        },
                         Message::AddDir(d) => {
+                            debug!("Got 'AddDir' Request: {:?}", &d);
                             let dir = d.to_string_lossy().to_string();
-                            // debug!("Adding Direcotry: {:?}", d);
                             shared.write().unwrap().push(Directory{
                                 name: dir.clone(),
                                 paths: vec![dir]
@@ -175,92 +211,63 @@ pub async fn run(self) -> (Sender<Message>, Receiver<Response>){
                             let _ = rtx.send(Response::Done(Message::AddDir(d))).await;
                         },
                         Message::RemoveDir(name) => {
-                            // debug!("Removing Direcotry: {:?}", &name);
+                            debug!("Got 'RemoveDir' Request: {:?}", &name);
                             shared.write().unwrap().retain(|d| d.name != name.to_string_lossy().to_string());
                             let _ = rtx.send(Response::Done(Message::RemoveDir(name))).await;
                         },
                         Message::SetNickname(n) => {
-                            // debug!("Current Nickname: {:?}", nickname.read().unwrap());                            // debug!("New Nickname: {:?}", a.lock().await.nickname);
-                            // debug!("Updating Nickname: {}", n);
+                            debug!("Got 'SetNickname' Request: {}", &n);
                             {                            
                                 *nickname.write().unwrap() = n.clone();
                             }
                             let _ = rtx.send(Response::Done(Message::SetNickname(n))).await;
                         },
                         Message::StartBroadcast => {
-                            // debug!("Starting Broadcast");
+                            debug!("Got 'StartBroadcast' Request");
                             let _ = rtx.send(Response::Done(Message::StartBroadcast)).await;
                         },
                         Message::StopBroadcast => {
-                            // debug!("Stopping Broadcast");
+                            debug!("Got 'StopBroadcast' Request");
                             let _ = rtx.send(Response::Done(Message::StartBroadcast)).await;
                         },
                         Message::StartListen => {
-                            // debug!("Starting Listen");
+                            debug!("Got 'StartListen' Request");
                             let _ = rtx.send(Response::Done(Message::StartListen)).await;
                         },
                         Message::StopListen => {
-                            // debug!("Stopping Listen");
+                            debug!("Got 'StopListen' Request");
                             let _ = rtx.send(Response::Done(Message::StopListen)).await;
                         },
                         Message::StartServe(addr) => {
-                            let (s_exit_tx, serve_exit_rx) = oneshot::channel::<()>();
-                            let rt = rtx.clone();
-                            let sv = sv.clone();
-                            tokio::spawn(async move {
+                            debug!("Got 'StartServe' Request: {:?}", &addr);
 
-                                let var_name = match serve(sv, addr, serve_exit_rx).await {
-                                    Ok(_) => rt.send(Response::Done(Message::StartServe(addr))).await,
-                                    Err(err) => rt.send(Response::Err("failed to serve".to_string())).await,
-                                };
-                            });
-                            // *serve_exit_tx.lock().await = Some(s_exit_tx); 
-                            // serve_exit_fn = Some(async move {
-                            //     s_exit_tx.send(())
-                            // }.into_future());
+                            let sv = sv.clone();
+
+                            serve_exit_tx = Some(serve(sv, addr.clone()));
+
+                            let _ = rtx.clone().send(Response::Done(Message::StartServe(addr))).await;
                         },
                         Message::StopServe => {
-                            // if serve_exit_tx.lock().await.is_some() {
-                            //     let mut t = serve_exit_tx.lock().await;
-                            //     let mut t = t.as_mut().unwrap();
-                            // // }
-                            // // if let Some(t) = serve_exit_tx {
-                            //     match t.send(()) {
-                            //         Ok(_) => {
-                            //             *serve_exit_tx.lock().await = None;
-                            //             let _ = rtx.send(Response::Done(Message::StopServe)).await;
-                            //         },
-                            //         Err(e) => {
-                            //             let _ = rtx.send(Response::Err(format!("{}: {:?}","failed to stop serving".to_string(), e))).await;
-                            //         }
-                            //     };
-                            // }
-                            // let t = serve_exit_tx.unwrap();
-                            // t.send(());
-
-                            // match serve_exit_tx {
-                            //     Some(t) => {
-                            //     },
-                            //     None => {
-                            //         let _ = rtx.send(Response::Err("currently not serving".to_string())).await;
-                            //     },
-                            // }
-                            // match serve_exit_fn {
-                            //     Some(ref f) => {
-                            //         match f.await {
-                            //             Ok(_) => {
-                            //                 let _ = rtx.send(Response::Done(Message::StopServe)).await;
-                            //             }
-                            //             Err(e) => {
-
-                            //             }
-                            //         }
-                            //     }
-                            //     None => {}
-                            // }
+                            debug!("Got 'StopServe' Request");
+                            if let Some(t) = serve_exit_tx.take() {
+                                match t.send(()) {
+                                    Ok(_) => {
+                                        let _ = rtx.send(Response::Done(Message::StopServe)).await;
+                                    }
+                                    Err(_) => {
+                                        let _ = rtx.send(Response::Err("failed to stop serving".to_string())).await;
+                                    }
+                                };
+                            }
                         },
                         Message::Exit => {
-                            // debug!("Exiting");
+                            debug!("SENDING EXIT REQUESTS...");
+                            let _ = tx.send(Message::StopListen).await;
+                            debug!("SEND EXIT REQUEST: StopListen");
+                            let _ = tx.send(Message::StopBroadcast).await;
+                            debug!("SEND EXIT REQUEST: StopBroadcast");
+                            let _ = tx.send(Message::StopServe).await;
+                            debug!("SEND EXIT REQUEST: StopServe");
                             let _ = rtx.send(Response::Done(Message::Exit)).await;
                             break;
                         },
@@ -276,69 +283,35 @@ pub async fn run(self) -> (Sender<Message>, Receiver<Response>){
 
 }
 
-#[tokio::test]
-async fn test_sv_new() {
+    
+#[allow(dead_code)]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("RUST_LOG", "DEBUG".to_string());
+    let _ = env_logger::init();
 
     let s = SvBuilder::new("blub".to_string(), vec!["dir_1".to_string(), "dir_2".to_string()]);
     let (t,mut r) = s.run().await;
-    // let t = s.manager_tx.clone();
-    let v = tokio::spawn(async move {
-            let _ = t.send(Message::AddDir(PathBuf::from("C:/Test1"))).await;
-            let _ = t.send(Message::RemoveDir(PathBuf::from("Test1"))).await;
-            let _ = t.send(Message::SetNickname("Nick_123".to_string())).await;
-            let _ = t.send(Message::StartServe(SocketAddr::new(Ipv4Addr::new(0,0,0,0).into(), 9001))).await;
-            let _ = t.send(Message::StopServe).await;
-            let _ = t.send(Message::StartBroadcast).await;
-            let _ = t.send(Message::StartBroadcast).await;
-            let _ = t.send(Message::StartListen).await;
-            let _ = t.send(Message::StopListen).await;
-            // let _ = t.send(Message::Exit).await;
-        });
+    let _ = t.send(Message::AddDir(PathBuf::from("C:/Test1"))).await;
+    let _ = t.send(Message::RemoveDir(PathBuf::from("Test1"))).await;
+    let _ = t.send(Message::SetNickname("Nick_123".to_string())).await;
+    let _ = t.send(Message::StartServe(SocketAddr::new(Ipv4Addr::new(0,0,0,0).into(), 9001))).await;
+    let _ = t.send(Message::StartBroadcast).await;
+    let _ = t.send(Message::StartListen).await;
+
+    thread::sleep(Duration::from_secs(20));
+
+    let _ = t.send(Message::Exit).await;
     
     while let Some(msg) = r.recv().await {
         match msg {
             Response::Done(m) => {
-                debug!("DONE = {:?}", m);
+                info!("DONE = {:?}", m);
             },
             Response::Err(e) => {
-                debug!("ERROR = {:?}", e);
+                info!("ERROR = {:?}", e);
             }
         };
-    }
-    v.await.unwrap();
-
-    }
-
-// struct Manager {
-//     rx: Receiver<Message>,
-//     // serve_tx: Sender<Message>,
-//     // broadcast_tx: Sender<Message>
-// }
-
-// impl Manager {
-//     pub fn new() -> Sender<Message> {
-//         let (tx, mut rx) = mpsc::channel::<Message>(32);
-
-//         let _ = tokio::spawn(async move {
-//             loop {
-//                 match rx.recv().await {
-//                     Some(msg) => {
-//                         match msg {
-//                             Message::AddDir(d) => debug!("Adding Direcotry: {:?}", d),
-//                             Message::RemoveDir(d) => debug!("Removing Direcotry: {:?}", d),
-//                             Message::SetNickname(n) => debug!("Updating Nickname: {}", n),
-//                             Message::Exit => {
-//                                 debug!("Exiting");
-//                                 break;
-//                             },
-//                         }
-//                     },
-//                     None => {
-//                     }
-//                 }
-//             }
-//         });
-
-//         tx
-//     }
-// }
+    };
+    Ok(())
+}
